03de86d7577592cb06a147ad2c6b0806b0325a45
vitaliy-kro <krochakv6@gmail.com>
2023-02-03 22:49:51 +0200

    change routes path, delete api word

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use('/api/avatars', express.static('public/avatars'));
+app.use('/avatars', express.static('public/avatars'));
 
-app.use('/api/contacts', contactsRouter);
-app.use('/api/users', authRouter);
+app.use('/contacts', contactsRouter);
+app.use('/users', authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
